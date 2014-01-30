f2fc9ab63dff1b9c7866df3eeeec30bc259b65a4
Dylan <dylan.pickel@gmail.com>
2014-01-30 10:49:55 -0800

    Variables, variables, variables

--- Main.asm
+++ Main.asm
@@ -11,11 +11,15 @@ DSEG at 30h
 	;PWM Variables - don't touch!
 	desiredTemp:	ds	1
 	timeToReach:	ds	1
-	minutes:		ds	1
-	seconds:		ds	1
+	UniMin:			ds	1
+	UniSec:			ds	1
+	TskMin:			ds	1
+	TskSec:			ds	1
+	count:			ds	1
 
 BSEG
 	donePWM:	dbit	1
+	holding:	dbit	1
 	
 CSEG
 
