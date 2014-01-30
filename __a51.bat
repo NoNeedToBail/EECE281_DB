23259b34cd2ab9329bf93da715e6cf5850ec435e
Dylan <dylan.pickel@gmail.com>
2014-01-30 11:16:13 -0800

    Delete __a51.bat

--- __a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\Dylan\Documents\GitHub\EECE281_DB\"
-"C:\Program Files\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Dylan\Documents\GitHub\EECE281_DB\PWMtest.asm"
-echo Crosside_Action Set_Hex_File C:\Users\Dylan\Documents\GitHub\EECE281_DB\PWMtest.HEX
