aec577f515298802081fad7498c9a187906ff13a
Dylan <dylan.pickel@gmail.com>
2014-01-30 09:20:16 -0800

    Documentation and variables for Main.asm

--- Main.asm
+++ Main.asm
@@ -1,7 +1,15 @@
 ;=============================================================================
-;Main.asm
+; Main.asm
+; UI code and runnable file for the reflow oven controller.
 ;=============================================================================
 
 $modde2
+
+DSEG at 30h
+	temperature:	ds	1
+	desiredTemp:	ds	1
+
+BSEG
+	donePWM:	dbit	1
 
 END
