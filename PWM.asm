7b86e44e56cd97772cfff46e3e80116e1010b958
Dylan <dylan.pickel@gmail.com>
2014-01-30 18:04:36 -0800

    Implemented Most Functionality

--- PWM.asm
+++ PWM.asm
@@ -4,23 +4,26 @@ $NOLIST
 ;==============================================================
 ; PWM.asm: Library of functions for setting temperature.
 ; Variables required:
+; Unimin, Unisec
+; Tskmin, Tsksec
 ; DesiredTemp
 ; Temperature
-; TimeToReach
 ; DonePWM
+; Holding
 ;==============================================================
 
 PWR EQU P0.0
 CLK EQU 33333333
 T0_Freq EQU 50
 T0_RELOAD EQU 65536-(CLK/(12*T0_Freq))
+RANGE EQU 5
 
 ;SetTemp - Ramps to desired temp then sets PWMDone
 
 SetTemp MAC
 	mov DesiredTemp, %0
 	clr pwmdone
-	
+	clr holding
 	ENDMAC
 	
 ;HoldTemp - holds the temp for the desired length then sets PWMDone
@@ -29,7 +32,7 @@ HoldTemp MAC
 	mov Tskmin, %0
 	mov tsksec, %1
 	clr pwmdone
-
+	setb holding
 	ENDMAC
 	
 ;PWMISR - ISR to deal with timing and temperature adjustments
@@ -43,13 +46,82 @@ PWMISR:
 	mov a, count
 	cjne a, #50, falseStart
 	mov count, #0
-	lcall incUNiTime
-	
+	lcall incUniTime
+	jb holding, hold
+Ramp:
+	lcall rampf
+Hold:
+	lcall holdf
+Done:
+	pop acc
+	pop psw
+	ret
 
 falseStart:
 	pop acc
 	pop psw
 	ret
+	
+;rampf - ISR function for when in ramping mode
+
+rampf:
+;my thoughts: call temp adjust
+;then check to see if temp is within acceptable range to switch to plateauing
+;do we care about the rate or are we assuming we're just hitting it? wait and see?
+;also need to turn off bit for PWMdone, should this be done @the start of hitting a plateau?
+;aka here in this function?
+	lcall tempadjust
+	clr c
+;maybe change this to be more efficient later?
+	mov a, Range
+	subb a, difference
+	jnc doneRamp
+	ret
+doneRamp:
+	setb PWMdone
+	ret
+	
+;holdf - ISR function for when in holding mode
+
+holdf:
+	lcall tempAdjust
+	mov a, TskSec
+	jnz NotZero
+	mov a, TskMin
+	jnz NotZero
+	setb PWMDone
+	clr holding
+	ret
+NotZero:
+	lcall decTskTime
+	ret
+	
+;tempAdjust - checks desired vs real temperature and sets or clears PWR
+	
+tempAdjust:
+	clr c
+	mov a, temperature
+	subb a, desiredtemp
+	mov difference, a
+	jb acc.7, 2comp
+pos:
+	jnc toohot
+toocold:
+	setb PWR
+	ret
+toohot:
+	clr PWR
+	ret
+2comp:
+	cpl a
+	inc a
+	mov difference, a
+	sjmp pos
+	
+;DecTskTime - decrements the time left for holding
+
+decTskTime:
+	;waiting on Alex's lab code
 	
 ;InitTimer0 - initialization for timer 0
 	
@@ -64,14 +136,10 @@ InitTimer0:
 	mov TL0, #low(T0_RELOAD)	;initial frequency of genereated function
 	setb TR0	;start timer 1
 	setb ET0
+	ret
+	
+;IncUniTime - increments the total time that the system has been running
 
-	ret
-	
-;AdjustTemp - Turns the heat on or off based on current temp and desired temp
-	
-AdjustTemp:
-	
-	ret
 incUniTime:
 	mov a, unisec
 	add a, #1
